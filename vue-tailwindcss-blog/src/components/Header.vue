1a7b7b9e688d2bbe2e4a4b3e6fb809106cf3c3b6
danielquiz1 <danielquiz1@gmail.com>
2025-06-16 13:25:48 -0700

    feat: Updated header desription

--- vue-tailwindcss-blog/src/components/Header.vue
+++ vue-tailwindcss-blog/src/components/Header.vue
@@ -11,8 +11,12 @@
                 <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#6a7282" scale="3"></v-icon></a>
                 <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#6a7282" scale="3"></v-icon></a>
             </div>
-            <h1 class="text-5xl font-extrabold mb-10 text-gray-500 cursor-pointer" @click="$router.push('/')">Daniel's Blog</h1>
-            <p class="font-extralight text-3xl text-gray-300">A blog to learn a little bit about vue & tailwind</p>
+            <h1 class="text-5xl font-extrabold mb-10 text-gray-500 cursor-pointer" @click="$router.push('/')">Daniel Gray</h1>
+            <p class="font-extralight text-xl text-gray-300">Built with 
+                <a class="underline text-gray-500 hover:text-blue-500 visited:text-purple-300" href="https://vuejs.org/">Vue</a>, 
+                <a class="underline text-gray-500 hover:text-blue-500 visited:text-purple-300" href="https://tailwindcss.com/">Tailwind CSS</a>, and 
+                <a class="underline text-gray-500 hover:text-blue-500 visited:text-purple-300" href="https://supabase.com/">Supabase</a>.
+            </p>
         </header>
     </div>
 </template>
