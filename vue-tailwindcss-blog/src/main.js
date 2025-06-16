4ed57f4e292305e17f53aaf1609950520f364a63
danielquiz1 <danielquiz1@gmail.com>
2025-06-16 09:41:23 -0700

    build: more font awesome setup

--- vue-tailwindcss-blog/src/main.js
+++ vue-tailwindcss-blog/src/main.js
@@ -4,7 +4,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// FontAwesome setup
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faArrowLeft)
+
 const app = createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
