147a604a9e97f5fca0bdbf2fcdb24c5a2c481a7a
danielquiz1 <danielquiz1@gmail.com>
2025-06-18 09:34:59 -0700

    fix: Changed header line to be more responsive using flex rather than grid

--- vue-tailwindcss-blog/src/components/Header.vue
+++ vue-tailwindcss-blog/src/components/Header.vue
@@ -1,12 +1,7 @@
 <template>
     <div class="container mx-auto">
         <header class="text-center my-8">
-            <div class="grid grid-cols-8 gap-4 m-4">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div class="m-4 flex flex-row-reverse">
                 <a href="https://github.com/grayd4"><v-icon name="bi-github" color="#6a7282" scale="3"></v-icon></a>
                 <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#6a7282" scale="3"></v-icon></a>
                 <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#6a7282" scale="3"></v-icon></a>
