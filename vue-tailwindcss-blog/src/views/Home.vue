f1d8728f00fee4e290db29a15f9b5c5d0691f1e8
danielquiz1 <danielquiz1@gmail.com>
2025-06-21 11:30:39 -0700

    feat: added thumbnails to tech side as well

--- vue-tailwindcss-blog/src/views/Home.vue
+++ vue-tailwindcss-blog/src/views/Home.vue
@@ -32,6 +32,7 @@ fetchPosts()
                         <h1 class="text-slate-900 text-2xl">{{ item.title }}</h1>
                         <p class="text-slate-300 text-xl mb-4">{{ new Date(item.date).toLocaleString("en-US") }}</p>
                         <p class="text-xl text-slate-500">{{ getWordCount(item.content) }} words</p>
+                        <img v-if="item.thumb" loading="lazy" class="rounded h-48 w-96 object-cover m-1" v-bind:src="item.thumb">
                     </div>
                 </div>
                 <div class="general-posts">
@@ -40,11 +41,9 @@ fetchPosts()
                         There are no posts.
                     </div>
                     <div v-else class="PostItem border border-slate-700 mb-4 p-4 rounded-lg cursor-pointer bg-slate-100" v-for="item, itemIndex in store.posts.filter((item) => item.category != 'tech')" v-bind:key="itemIndex" @click="$router.push(`/post/${item.id}`)">
-                        <div>
-                            <h1 class="text-slate-900 text-2xl">{{ item.title }}</h1>
-                            <p class="text-slate-300 text-xl mb-4">{{ new Date(item.date).toLocaleString("en-US") }}</p>
-                            <p class="text-xl text-slate-500">{{ getWordCount(item.content) }} words</p>
-                        </div>
+                        <h1 class="text-slate-900 text-2xl">{{ item.title }}</h1>
+                        <p class="text-slate-300 text-xl mb-4">{{ new Date(item.date).toLocaleString("en-US") }}</p>
+                        <p class="text-xl text-slate-500">{{ getWordCount(item.content) }} words</p>
                         <img v-if="item.thumb" loading="lazy" class="rounded h-48 w-96 object-cover m-1" v-bind:src="item.thumb">
                     </div>
                 </div>
