6cb8092e544ffea78d46bc3717066b3ffe3734f7
grayd4 <grayd4@spu.edu>
2025-06-26 14:42:26 -0700

    feat: header background issue

--- vue-tailwindcss-blog/src/components/Header.vue
+++ vue-tailwindcss-blog/src/components/Header.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="container mx-auto">
-        <header class="text-center my-8">
-            <div class="m-4 flex flex-row-reverse">
-                <a href="https://github.com/grayd4"><v-icon name="bi-github" color="#6a7282" scale="3"></v-icon></a>
-                <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#6a7282" scale="3"></v-icon></a>
-                <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#6a7282" scale="3"></v-icon></a>
-            </div>
-            <h1 class="text-5xl font-extrabold mb-10 text-gray-500 cursor-pointer" @click="$router.push('/')">Daniel Gray</h1>
-            <p class="font-extralight text-xl text-gray-300">Built with 
-                <a class="underline text-gray-500 hover:text-blue-500 visited:text-purple-300" href="https://vuejs.org/">Vue</a>, 
-                <a class="underline text-gray-500 hover:text-blue-500 visited:text-purple-300" href="https://tailwindcss.com/">Tailwind CSS</a>, and 
-                <a class="underline text-gray-500 hover:text-blue-500 visited:text-purple-300" href="https://supabase.com/">Supabase</a>.
-            </p>
-        </header>
+    <div class="bg-[url(/Background.png)] bg-cover bg-center bg-no-repeat">
+        <div class="container mx-auto">
+            <header class="text-center">
+                <div>
+                <div class="pt-4 px-4 flex flex-row-reverse">
+                    <a href="https://github.com/grayd4"><v-icon name="bi-github" color="#e5e7eb" scale="3"></v-icon></a>
+                    <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#e5e7eb" scale="3"></v-icon></a>
+                    <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#e5e7eb" scale="3"></v-icon></a>
+                </div>
+                <h1 class="text-5xl font-extrabold p-20 text-gray-200 cursor-pointer text-shadow-lg" @click="$router.push('/')">Daniel Gray</h1>
+                </div>
+            </header>
+        </div>
     </div>
+    <p class="font-extralight text-xl text-gray-500 text-center">Built with 
+        <a class="underline text-gray-600 hover:text-blue-500 visited:text-purple-300" href="https://vuejs.org/">Vue</a>, 
+        <a class="underline text-gray-600 hover:text-blue-500 visited:text-purple-300" href="https://tailwindcss.com/">Tailwind CSS</a>, and 
+        <a class="underline text-gray-600 hover:text-blue-500 visited:text-purple-300" href="https://supabase.com/">Supabase</a>.
+    </p>
 </template>
