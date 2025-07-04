d0de13797401c0b74f028c04ae08cc30e80267c8
danielquiz1 <danielquiz1@gmail.com>
2025-07-04 09:47:15 -0700

    fix: Make footer content smaller

--- vue-tailwindcss-blog/src/components/Footer.vue
+++ vue-tailwindcss-blog/src/components/Footer.vue
@@ -3,10 +3,10 @@
         <footer class="pt-4 pb-4">
             <div>
                 <div class="pt-4 px-4 flex flex-row-reverse">
-                    <a href="https://github.com/grayd4"><v-icon name="bi-github" color="#d1d5dc" scale="3"></v-icon></a>
-                    <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#d1d5dc" scale="3"></v-icon></a>
-                    <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#d1d5dc" scale="3"></v-icon></a>
-                    <p class="text-xl font-extrabold text-gray-300 w-full m-auto">Copyright Daniel Gray {{ new Date().getFullYear() }}</p>
+                    <a href="https://github.com/grayd4"><v-icon name="bi-github" color="#d1d5dc" scale="2"></v-icon></a>
+                    <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#d1d5dc" scale="2"></v-icon></a>
+                    <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#d1d5dc" scale="2"></v-icon></a>
+                    <p class="text-l font-extrabold text-gray-300 w-full m-auto">Copyright Daniel Gray {{ new Date().getFullYear() }}</p>
                 </div>
             </div>
         </footer>
