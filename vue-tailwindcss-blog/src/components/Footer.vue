164a498a0ebb031d9aa11e17ffc0155080eb72ae
danielquiz1 <danielquiz1@gmail.com>
2025-07-05 15:51:28 -0700

    feat: cleaned up contact page

--- vue-tailwindcss-blog/src/components/Footer.vue
+++ vue-tailwindcss-blog/src/components/Footer.vue
@@ -6,6 +6,7 @@
                     <a href="https://github.com/grayd4"><v-icon name="bi-github" color="#d1d5dc" scale="2"></v-icon></a>
                     <a href="https://www.linkedin.com/in/danieldgray/"><v-icon name="bi-linkedin" color="#d1d5dc" scale="2"></v-icon></a>
                     <a href="https://www.youtube.com/@daniel_gray"><v-icon name="bi-youtube" color="#d1d5dc" scale="2"></v-icon></a>
+                    <v-icon class="cursor-pointer" name="bi-envelope-fill" color="#d1d5dc" scale="2"  @click="$router.push('/Contact')"></v-icon>
                     <p class="text-l font-extrabold text-gray-300 w-full m-auto">Copyright Daniel Gray {{ new Date().getFullYear() }}</p>
                 </div>
             </div>
--- vue-tailwindcss-blog/src/views/Contact.vue
+++ vue-tailwindcss-blog/src/views/Contact.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="Contact">
         <main class="container mx-auto">
-            <ul>
-                <li><a href="danielquiz1@gmail.com">Email</a></li>
-                <li><a href="https://www.linkedin.com/in/danieldgray/">LinkedIn</a></li>
+            <p class="text-slate-500 m-4 text-xl pt-4">Feel free to reach out to me at:</p>
+            <ul class="list-disc text-slate-500 m-4 text-l">
+                <li>Email: <a class="underline text-gray-600 hover:text-blue-500 visited:text-purple-300" href="mailto:danielquiz1@gmail.com">danielquiz1@gmail.com</a></li>
+                <li><a class="underline text-gray-600 hover:text-blue-500 visited:text-purple-300" href="https://www.linkedin.com/in/danieldgray/">LinkedIn</a></li>
             </ul>
         </main>
-    </div>
 </template>
